8418e995574dd8327e0e9d511706bd82b8b06848
Roshan Kumar <67013997+roshanwaa@users.noreply.github.com>
2023-10-21 00:47:50 +0530

    add copyright tag

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <p className="copyRight">© roshan.waa</p>
   </React.StrictMode>
 );
 
